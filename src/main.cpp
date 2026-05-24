797d1e7899dc6a700061a746db6a18533db3a58e
Adarsh Rai <adarshrai409@gmail.com>
2026-05-24 21:47:36 +0530

    refatored and added feature of pipes

--- src/main.cpp
+++ src/main.cpp
@@ -14,6 +14,7 @@
 #include "autocomplete.hpp"
 #include "builtin.hpp"
 #include "pathfind.hpp"
+#include <array>
 
 std::vector<std::string> parse(const std::string &line)
 {
@@ -82,6 +83,15 @@ std::vector<std::string> parse(const std::string &line)
           in_single_quotes = false;
         }
       }
+    } 
+    else if (c == '|' && (!in_single_quotes && !in_double_quotes)) 
+    {
+        if (!word.empty()) 
+        {
+            token.push_back(word);
+            word.clear();
+        }
+        token.push_back("|");
     }
     else if (std::isspace(c) && (!in_single_quotes && !in_double_quotes))
     {
@@ -106,6 +116,163 @@ std::vector<std::string> parse(const std::string &line)
 }
 
 std::vector<std::string> path_commands;
+
+bool executeCommand(
+    std::vector<std::string> &clean_tokens,
+    int stdout_index,
+    int stderr_index,
+    bool stdout_append,
+    bool stderr_append,
+    std::string &stdout_file,
+    std::string &stderr_file
+)
+{
+    std::string command = clean_tokens[0];
+
+    if (builtin.count(command))
+    {
+      int saved_stdout = -1;
+      int saved_stderr = -1;
+      if (stdout_index != -1)
+      {
+        int flags = O_WRONLY | O_CREAT | (stdout_append ? O_APPEND : O_TRUNC);
+        int fd = open(stdout_file.c_str(), flags, 0644);
+        saved_stdout = dup(STDOUT_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        close(fd);
+      }
+
+      if (stderr_index != -1) 
+      {
+        int flags = O_WRONLY | O_CREAT | (stderr_append ? O_APPEND : O_TRUNC);
+        int fd = open(stderr_file.c_str(), flags, 0644);
+        saved_stderr = dup(STDERR_FILENO);
+        dup2(fd, STDERR_FILENO);
+        close(fd);
+      }
+
+      if (command == "exit")
+      {
+        return false;
+      }
+      if (command == "echo")
+      {
+        for (int i = 1; i < clean_tokens.size(); i++)
+        {
+          std::cout << clean_tokens[i];
+          if (i + 1 < clean_tokens.size()) 
+          {
+              std::cout << ' ';
+          }
+        }
+        std::cout << std::endl;
+      }
+      else if (command == "type")
+      {
+        std::string check = "";
+        if (clean_tokens.size() > 1)
+        {
+          check = clean_tokens[1];
+        }
+        if (builtin.count(check))
+        {
+          std::cout << check << ' ' << "is a shell builtin" << std::endl;
+        }
+        else
+        {
+          fs::path file_path = findPath(check);
+          if (!file_path.empty())
+          {
+            std::cout << check << " is " << file_path.string() << std::endl;
+          }
+          else
+          {
+            std::cout << check << ": not found" << std::endl;
+          }
+        }
+      }
+      else if (command == "pwd")
+      {
+        std::cout << fs::current_path().c_str() << std::endl;
+      }
+      else if (command == "cd")
+      {
+        if (clean_tokens.size() < 2)
+        {
+          return true;
+        }
+        if (clean_tokens[1] == "~")
+        {
+          clean_tokens[1] = getenv("HOME");
+        }
+        if (chdir(clean_tokens[1].c_str()) != 0)
+        {
+          std::cout << clean_tokens[1].c_str() << ": No such file or directory" << std::endl;
+        }
+      }
+
+      if (stdout_index != -1)
+      {
+        dup2(saved_stdout, STDOUT_FILENO);
+        close(saved_stdout);
+      }
+      if (stderr_index != -1)
+      {
+        dup2(saved_stderr, STDERR_FILENO);
+        close(saved_stderr);
+      }
+    }
+    else
+    {
+      fs::path file_path = findPath(command);
+      if (!file_path.empty())
+      {
+        std::vector<char *> argv;
+        for (int i = 0; i < clean_tokens.size(); i++)
+        {
+          argv.push_back(const_cast<char *>(clean_tokens[i].c_str()));
+        }
+        argv.push_back(nullptr);
+
+        pid_t pid = fork();
+        if (pid == 0)
+        {
+          if (stdout_index != -1)
+          {
+            int flags = O_WRONLY | O_CREAT | (stdout_append ? O_APPEND : O_TRUNC);
+            int fd = open(stdout_file.c_str(), flags, 0644);
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+          }
+          if (stderr_index != -1)
+          {
+            int flags = O_WRONLY | O_CREAT | (stderr_append ? O_APPEND : O_TRUNC);
+            int fd = open(stderr_file.c_str(), flags, 0644);
+            dup2(fd, STDERR_FILENO);
+            close(fd);
+          }
+          execvp(file_path.c_str(), argv.data());
+          perror("exec failed");
+          exit(1);
+        }
+        else if (pid > 0)
+        {
+          int status;
+          waitpid(pid, &status, 0);
+        }
+        else
+        {
+          perror("fork failed");
+        }
+      }
+      else
+      {
+        std::cout << command << ": command not found" << std::endl;
+      }
+    }
+
+    return true;
+}
 
 int main()
 {
@@ -176,145 +343,104 @@ int main()
       }
       clean_tokens.push_back(tokens[i]);
     }
-    std::string command = clean_tokens[0];
-
-    if (builtin.count(command))
+    
+    bool is_pipes = false;
+    for (auto tk : clean_tokens) 
     {
-      int saved_stdout = -1;
-      int saved_stderr = -1;
-      if (stdout_index != -1)
-      {
-        int flags = O_WRONLY | O_CREAT | (stdout_append ? O_APPEND : O_TRUNC);
-        int fd = open(stdout_file.c_str(), flags, 0644);
-        saved_stdout = dup(STDOUT_FILENO);
-        dup2(fd, STDOUT_FILENO);
-        close(fd);
-      }
-
-      if (stderr_index != -1) 
-      {
-        int flags = O_WRONLY | O_CREAT | (stderr_append ? O_APPEND : O_TRUNC);
-        int fd = open(stderr_file.c_str(), flags, 0644);
-        saved_stderr = dup(STDERR_FILENO);
-        dup2(fd, STDERR_FILENO);
-        close(fd);
-      }
-
-      if (command == "exit")
-      {
-        break;
-      }
-      if (command == "echo")
-      {
-        for (int i = 1; i < clean_tokens.size(); i++)
+        if (tk == "|") 
         {
-          std::cout << clean_tokens[i] << ' ';
+            is_pipes = true;
+            break;
         }
-        std::cout << std::endl;
-      }
-      else if (command == "type")
-      {
-        std::string check = "";
-        if (clean_tokens.size() > 1)
-        {
-          check = clean_tokens[1];
-        }
-        if (builtin.count(check))
-        {
-          std::cout << check << ' ' << "is a shell builtin" << std::endl;
-        }
-        else
-        {
-          fs::path file_path = findPath(check);
-          if (!file_path.empty())
-          {
-            std::cout << check << " is " << file_path.string() << std::endl;
-          }
-          else
-          {
-            std::cout << check << ": not found" << std::endl;
-          }
-        }
-      }
-      else if (command == "pwd")
-      {
-        std::cout << fs::current_path().c_str() << std::endl;
-      }
-      else if (command == "cd")
-      {
-        if (clean_tokens.size() < 2)
-        {
-          continue;
-        }
-        if (clean_tokens[1] == "~")
-        {
-          clean_tokens[1] = getenv("HOME");
-        }
-        if (chdir(clean_tokens[1].c_str()) != 0)
-        {
-          std::cout << clean_tokens[1].c_str() << ": No such file or directory" << std::endl;
-        }
-      }
-
-      if (stdout_index != -1)
-      {
-        dup2(saved_stdout, STDOUT_FILENO);
-        close(saved_stdout);
-      }
-      if (stderr_index != -1)
-      {
-        dup2(saved_stderr, STDERR_FILENO);
-        close(saved_stderr);
-      }
     }
-    else
+
+    if (is_pipes) 
     {
-      fs::path file_path = findPath(command);
-      if (!file_path.empty())
-      {
-        std::vector<char *> argv;
-        for (int i = 0; i < clean_tokens.size(); i++)
+        std::vector<std::vector<std::string>> commands;
+        std::vector<std::string> current; 
+        for (int i = 0; i < clean_tokens.size(); i++) 
         {
-          argv.push_back(const_cast<char *>(clean_tokens[i].c_str()));
+            if (clean_tokens[i] == "|")
+            {
+                commands.push_back(current);
+                current.clear();
+            }
+            else 
+            {
+                current.push_back(clean_tokens[i]);
+            }
         }
-        argv.push_back(nullptr);
+        if (!current.empty())
+        {
+            commands.push_back(current);
+        }
+        
+        int n = commands.size();
 
-        pid_t pid = fork();
-        if (pid == 0)
+        std::vector<std::array<int, 2>> pipes(n - 1);
+
+        for (int i = 0; i < n - 1; i++)
         {
-          if (stdout_index != -1)
-          {
-            int flags = O_WRONLY | O_CREAT | (stdout_append ? O_APPEND : O_TRUNC);
-            int fd = open(stdout_file.c_str(), flags, 0644);
-            dup2(fd, STDOUT_FILENO);
-            close(fd);
-          }
-          if (stderr_index != -1)
-          {
-            int flags = O_WRONLY | O_CREAT | (stderr_append ? O_APPEND : O_TRUNC);
-            int fd = open(stderr_file.c_str(), flags, 0644);
-            dup2(fd, STDERR_FILENO);
-            close(fd);
-          }
-          execvp(file_path.c_str(), argv.data());
-          perror("exec failed");
-          exit(1);
+            if (pipe(pipes[i].data()) == -1)
+            {
+                perror("pipe");
+                continue;
+            }
         }
-        else if (pid > 0)
+
+        std::vector<pid_t> pids;
+
+        for (int i = 0; i < n; i++)
         {
-          int status;
-          waitpid(pid, &status, 0);
+            pid_t pid = fork();
+
+            if (pid == 0)
+            {
+                if (i > 0)
+                {
+                    dup2(pipes[i - 1][0], STDIN_FILENO);
+                }
+
+                if (i < n - 1)
+                {
+                    dup2(pipes[i][1], STDOUT_FILENO);
+                }
+
+                for (int j = 0; j < n - 1; j++)
+                {
+                    close(pipes[j][0]);
+                    close(pipes[j][1]);
+                }
+                executeCommand(commands[i], stdout_index, stderr_index, stdout_append, stderr_append, stdout_file, stderr_file);  
+                exit(0);
+            }
+            else if (pid > 0)
+            {
+                pids.push_back(pid);
+            }
+            else
+            {
+                perror("fork");
+            }
         }
-        else
+
+        for (int i = 0; i < n - 1; i++)
         {
-          perror("fork failed");
-          continue;
+            close(pipes[i][0]);
+            close(pipes[i][1]);
         }
-      }
-      else
-      {
-        std::cout << command << ": command not found" << std::endl;
-      }
+
+        for (auto pid : pids)
+        {
+            waitpid(pid, nullptr, 0);
+        }
+    } 
+    else 
+    {
+        if (!executeCommand(clean_tokens, stdout_index, stderr_index, stdout_append, stderr_append, stdout_file, stderr_file)) {
+            break;
+        }
     }
+
   }
 }
